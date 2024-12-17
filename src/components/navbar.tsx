f9eb1d5bb7c368a96ec18968d2188beae1256dea
JuanBaut <deserd@protonmail.com>
2024-12-17 12:55:20 -0500

    updating tailwind

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,21 @@
+import { MenuIcon } from "lucide-react";
 import Links from "./links";
 
 export default function Navbar() {
   return (
-    <nav className="fixed flex h-20 w-full px-4 backdrop-blur">
+    <nav className="fixed flex h-20 w-full px-8 backdrop-blur">
       <div className="flex w-full justify-between">
-        <div className="self-center text-start text-5xl font-medium tracking-tighter text-primary">
+        <div className="self-center text-start text-5xl font-medium tracking-tighter text-primary max-sm:text-xl">
           Joseph Carreno
         </div>
 
-        <div className="flex justify-center gap-4">
+        <div className="invisible flex justify-center gap-4">
           <Links name="Home" href="#"></Links>
           <Links name="Works" href="#"></Links>
           <Links name="Contact" href="#"></Links>
+        </div>
+        <div className="hidden self-center">
+          <MenuIcon />
         </div>
       </div>
     </nav>
